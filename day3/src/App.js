5c23370c962910d2006bedd3c1a39f757dd8ac38
MuratFatih_FEdev_ _ <you@example.com>
2023-07-19 21:04:56 +0200

    solved the question

--- day3/src/App.js
+++ day3/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState("");
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleClick = () => setInputValue("")
+  const handleH2MouseOver = () => console.log(inputValue);
+  const handleInputChange = (e) => setInputValue(e.target.value);
+  const handleInputFocus = () => setIsInputFocused(true);
+  const handleInputBlur = () => setIsInputFocused(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onClick={handleClick}>Click me to clear the input</h1>
+      <h2 onMouseOver={handleH2MouseOver}>Hover me to log what is in the input to the console</h2>
+      <input
+        value={inputValue}
+        onChange={handleInputChange}
+        onFocus={handleInputFocus} 
+        onBlur={handleInputBlur} 
+        placeholder="type something here to show in the box below" 
+      />
+
+      <br />
+      <br />
+
+      <div>
+        {/* when the input is focused a message should be rendered : "user typing ..." */}
+        {isInputFocused ? "user typing...." : null}
+      </div>
+
+      <div>
+        {/* the input value should show here 
+            if the input is empty show a message :"nothig to show!"
+        */}
+
+        {inputValue ? inputValue : "Nothing to show here!"}
+      </div>
     </div>
   );
 }
